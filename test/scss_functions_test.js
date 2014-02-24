ad337e5fc9c8a69595a3c7fbf62c67090dc157ef
Kyo Nagashima <kyo@hail2u.net>
2014-02-24 17:59:32 +0900

    Update test script

--- test/scss_functions_test.js
+++ test/scss_functions_test.js
@@ -4,53 +4,56 @@ var grunt = require('grunt');
 var path = require('path');
 
 var dirActual = path.join(__dirname, '..', 'tmp');
-var dirExpected = path.join(__dirname, 'expected');
+var dirExpected = path.join(__dirname, 'fixtures');
+var actual;
+var expected;
 
 exports.scss_functions = {
-  setUp: function (done) {
-    done();
-  },
   color: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read(path.join(dirActual, 'color.css'));
-    var expected = grunt.file.read(path.join(dirExpected, 'color.css'));
+    actual = grunt.file.read(path.join(dirActual, 'color.css'));
+    expected = grunt.file.read(path.join(dirExpected, 'color.css'));
     test.strictEqual(actual, expected);
 
     test.done();
   },
+
   list: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read(path.join(dirActual, 'list.css'));
-    var expected = grunt.file.read(path.join(dirExpected, 'list.css'));
+    actual = grunt.file.read(path.join(dirActual, 'list.css'));
+    expected = grunt.file.read(path.join(dirExpected, 'list.css'));
     test.strictEqual(actual, expected);
 
     test.done();
   },
+
   misc: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read(path.join(dirActual, 'misc.css'));
-    var expected = grunt.file.read(path.join(dirExpected, 'misc.css'));
+    actual = grunt.file.read(path.join(dirActual, 'misc.css'));
+    expected = grunt.file.read(path.join(dirExpected, 'misc.css'));
     test.strictEqual(actual, expected);
 
     test.done();
   },
+
   number: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read(path.join(dirActual, 'number.css'));
-    var expected = grunt.file.read(path.join(dirExpected, 'number.css'));
+    actual = grunt.file.read(path.join(dirActual, 'number.css'));
+    expected = grunt.file.read(path.join(dirExpected, 'number.css'));
     test.strictEqual(actual, expected);
 
     test.done();
   },
+
   string: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read(path.join(dirActual, 'string.css'));
-    var expected = grunt.file.read(path.join(dirExpected, 'string.css'));
+    actual = grunt.file.read(path.join(dirActual, 'string.css'));
+    expected = grunt.file.read(path.join(dirExpected, 'string.css'));
     test.strictEqual(actual, expected);
 
     test.done();
